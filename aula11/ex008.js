72b226a9eea1800e5c5bca303c6ce33350c00940
RuanDSouza <danilosouzah50@gmail.com>
2025-06-23 22:34:32 -0300

    aula de condicional

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 66.5
+console.log(`a volocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //CONDIÇÃO SIMPLES    
+    console.log('você ultrapassou a velocidade permitidade. MULTADO!')
+}
+console.log('dirija sempre usando o cinto de segurança!')
