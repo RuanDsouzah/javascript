763816bc9ad8ba27f6156efed5b4e8bb9c0c11a9
RuanDSouza <danilosouzah50@gmail.com>
2025-07-10 17:48:25 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,11 +10,41 @@ function verificar() {
         var idade = ano - Number(fano.value)
         res.innerHTML = `idade calculada ${idade}`
         var genero = ''
-        if (fsex[0].checked) {
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex [0].checked){
             genero = 'homem'
-        } else if (fsex[1].checked) {
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'H_bebe.png')
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'H_jovem.png')
+            }else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'H_adulto.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'H_idoso.png')
+            }
+        } else if (fsex[1].checked){
             genero = 'mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'M_bebe.png')
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'M_jovem.png')
+            }else if(idade < 50){
+                //adulto
+                img.setAttribute('src', 'M_adulta.png')
+            }else {
+                //idoso
+                img.setAttribute('src', 'M_idosa.png')
+            }
         }
-        res.innerHTML = `detectamos genero`
+        res.style.textAlign = 'center'
+        res.innerHTML = `detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
